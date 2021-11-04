5f8d6cd48893b3ec4de8bccf303545cf55e7d267
toanho <toanho1104@gmail.com>
2021-11-04 12:43:33 +0700

    create bottomTabBar

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react'
 import {
   View, Dimensions, SafeAreaView, Text, TextInput, StyleSheet, TouchableOpacity, Alert, Image,
 } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { AssetScreen, MarketScreen } from '../screens'
+import BottomTabBarRoute from './bottomTab/bootomTabBarRoutes'
+import { screenName } from '../configs'
+
+const Stack = createNativeStackNavigator()
 
 const RootRoute = () => {
   return (
-    <View>
-      <Text>RootRoute</Text>
-      <AssetScreen />
-      <MarketScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={screenName.BottomTabBarRoute} component={BottomTabBarRoute} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,11 @@
 import AssetScreen from './assetScreen'
 import MarketScreen from './marketScreen'
+import DiscoverScreen from './discoverScreen'
+import MyScreen from './myScreen'
 
 export {
   AssetScreen,
   MarketScreen,
+  DiscoverScreen,
+  MyScreen,
 }
